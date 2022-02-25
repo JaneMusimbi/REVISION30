4dd4ee30329d73443792b9889818c5bc9e83b7e3
JaneMusimbi <janemusimbi@gmail.com>
2022-02-25 11:21:59 -0500

    fourth commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main(){
+    var x=fact("i bought 6 mangoes")
+    println(x)
+    var d=fullname ("jane" ,"musimbi")
+    println(d)
+    var y=(200 % 15 % 6 % 3)
+    println(y)
+    var m=sum(45,30,13,76,98,46)
+    println(m)
+    var v=("kenyan")
+    println(v)
+
+}
+fun fact(name:String):String{
+    var fruits=("i bought 6 mangoes")
+    return(fruits)
+}
+
+fun fullname(firstname:String,secondname:String):String{
+    var y=( "jane" +" " + "musimbi")
+    return(y)
+
+}
+fun modulus(num1:Int,num2:Int,num3:Int,num4:Int):Int{
+    var w=(num1 % num2 % num3 % num4)
+    return(w)
+}
+
+fun sum(num1:Int,num2:Int,num3:Int,num4:Int,num5:Int,num6:Int):Int{
+    var add=(num1 + num2 + num3 + num4 + num5 + num6)
+    return(add)
+}
+
+fun nationality(name1:String):String{
+    val Bolean=("true")
+    return(Bolean)
+}
